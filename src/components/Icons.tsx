ba2cad7a019deae9d6ecec969f81c5403eb92848
markangelosab <markangelosab@gmail.com>
2025-09-02 22:03:36 +0800

    update

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -23,3 +23,15 @@ export function FemaleIcon() {
     </div>
   );
 }
+
+// declarations.d.ts
+declare namespace JSX {
+  interface IntrinsicElements {
+    'lord-icon': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
+      src?: string;
+      trigger?: string;
+      colors?: string;
+      style?: React.CSSProperties;
+    };
+  }
+}
